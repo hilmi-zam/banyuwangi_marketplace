9cfe27c43b1661a6d40a7487a10cd12ee00817d7
damput-temp <danilamrull@gmail.com>
2025-12-17 14:46:01 +0700

    membuat branch vendor b
    git commit --amend --author="damput-temp <danilamrull@gmail.com>"

--- routes/vendorRoute.js
+++ routes/vendorRoute.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { getVendorA } from '../vendor/vendorA.js';
+import { getVendorB } from '../vendor/vendorB.js';
 import { getVendorC } from '../vendor/vendorC.js';
 const vendorRoute = Router();
 
 vendorRoute.get('/vendor-a', getVendorA);
+vendorRoute.get('/vendor-b', getVendorB);
 vendorRoute.get('/vendor-c', getVendorC);
 
 export default vendorRoute;
