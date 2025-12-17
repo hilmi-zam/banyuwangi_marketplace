88733d1280f5f5567dfaf17f61eb32b4166e9079
MuhAinulHuda24 <mainulhuda2005@gmail.com>
2025-12-17 14:35:53 +0700

    tambah produk

--- vendor/vendorA.js
+++ vendor/vendorA.js
@@ -4,6 +4,13 @@ export const getVendorA = (req, res) => {
         nm_brg: "Kopi Bubuk 100g",
         hrg: "15000",
         ket_stok: "ada"
-    }];
+    },
+    {
+        kd_produk: "A002",
+        nm_brg: "Kripik Singkong 200g",
+        hrg: "10000",
+        ket_stok: "ada"
+    }
+];
     res.json(data);
 }
